f04feeef4c202d1608be5c21352bb43ea5c5e9f8
PehlivanMert <pehlivannmert@gmail.com>
2024-06-08 00:46:35 +0300

    Added Report

--- src/Pages/Report/Report.jsx
+++ src/Pages/Report/Report.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import DeleteIcon from "@mui/icons-material/Delete";
 import UpdateIcon from "@mui/icons-material/Update";
 import Modal from "../Modal/Modal";
